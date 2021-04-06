6df44608f0921307118ceefac87648cc5e1b4dfd
Joaribero <joaribero_la12@hotmail.com>
2021-04-06 19:38:29 -0300

    Delete dates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import Formulario from './components/Formulario.jsx';
 import Cita from './components/Cita.jsx';
 
@@ -9,10 +9,25 @@ function App() {
   //Array de citas
   const [citas, guardarCitas] = useState([]);
 
+  //UseEffect
+  useEffect(() => {
+    console.log('listo');
+  })
+
   //Función que toma las citas actuales y agrega la nueva
   const crearCita = cita => {
     guardarCitas([...citas, cita]);
   }
+
+  //Función para eliminar una cita
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+
+    guardarCitas(nuevasCitas);
+  }
+
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus Citas';
 
   return (
     <Fragment>
@@ -26,13 +41,16 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
-            {citas.map(cita => (
-              <Cita
-                key= {cita.id}
-                cita={cita}
-              />
-            ))}
+            <h2>{titulo}</h2>
+            {
+              citas.map(cita => (
+                <Cita
+                  key= {cita.id}
+                  cita={cita}
+                  eliminarCita={eliminarCita}
+                />
+              ))
+            }
           </div>
         </div>
       </div>
